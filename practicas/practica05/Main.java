8247f97812cfe081898d9bf14348f93ecab75324
gaelbensusseng <gaelbensussen@ciencias.unam.mx>
2025-05-15 21:32:02 -0600

    Modificando el Main de nuevo

--- practicas/practica05/Main.java
+++ practicas/practica05/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        //generando nuestras figuras, el orden de los datos del constructor es: lado1, lado2, lado3, base, altura
         FiguraGeometrica t1 = new Triangulo(3, 4, 5, 3, 4);
         FiguraGeometrica t2 = new Triangulo(5, 5, 5, 5, 4.33);
         FiguraGeometrica r  = new Rectangulo(5, 10);
