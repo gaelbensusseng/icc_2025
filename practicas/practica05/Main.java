6dbb8ac7819d7700724fbe1ef7b73dd8821206e7
gaelbensusseng <gaelbensussen@ciencias.unam.mx>
2025-05-15 21:26:27 -0600

    ajustando Main

--- practicas/practica05/Main.java
+++ practicas/practica05/Main.java
@@ -5,16 +5,21 @@ public class Main {
         FiguraGeometrica r  = new Rectangulo(5, 10);
         FiguraGeometrica c  = new Cuadrado(4);
 
-        // Imprimir cada figura sin usar colecciones ni bucles avanzados
+        // Imprimimos las salidas
+
+        //Triangulo 1: 
         System.out.println(t1);
         System.out.printf("Área=%.2f, Perímetro=%.2f%n", t1.area(), t1.perimetro());
 
+        //Triangulo 2:
         System.out.println(t2);
         System.out.printf("Área=%.2f, Perímetro=%.2f%n", t2.area(), t2.perimetro());
 
+        //Rectangulo 1: 
         System.out.println(r);
         System.out.printf("Área=%.2f, Perímetro=%.2f%n", r.area(), r.perimetro());
 
+        //Cuadrado 1: 
         System.out.println(c);
         System.out.printf("Área=%.2f, Perímetro=%.2f%n", c.area(), c.perimetro());
     }
